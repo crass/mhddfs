51fc91e5aa76a75620b4cfecbb16b1d6ab12d387
dimka <dimka@d7d42532-9e51-4b1d-b199-9e4c94b77a30>
2008-03-20 08:17:21 +0000

    забыл
    git-svn-id: http://svn.uvw.ru/mhddfs/trunk@48 d7d42532-9e51-4b1d-b199-9e4c94b77a30

---
+++ src/debug.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include "debug.h"
+#include "parse_options.h"
+
+int debug_level=MHDD_DEFAULT_DEBUG_LEVEL;
+
+int mhdd_debug(int level, const char *fmt, ...)
+{
+  if (level<=debug_level) return 0;
+  if (!mhdd.debug) return 0;
+  
+  char tstr[64];
+  time_t t=time(0);
+  struct tm *lt;
+  lt=localtime(&t);
+  strftime(tstr, 64, "%Y-%m-%d %H:%M:%S", lt);
+  fprintf(mhdd.debug, "mhddfs [%s]", tstr);
+
+  switch(level)
+  {
+    case MHDD_DEBUG: fprintf(mhdd.debug, " (debug): "); break;
+    case MHDD_INFO:  fprintf(mhdd.debug, " (info): ");  break;
+    default:         fprintf(mhdd.debug, ": ");  break;
+  }
+  
+  va_list ap;
+  va_start(ap, fmt);
+  int res=vfprintf(mhdd.debug, fmt, ap);
+  va_end(ap);
+  return res;
+}
+
---
+++ src/debug.h
@@ -0,0 +1,15 @@
+#ifndef __MHDD_DEBUG_H__
+#define __MHDD_DEBUG_H__
+
+#define MHDD_DEFAULT_DEBUG_LEVEL 2
+
+#define MHDD_DEBUG  0
+#define MHDD_INFO   1
+#define MHDD_MSG    2
+
+
+int mhdd_debug(int level, const char *fmt, ...);
+
+extern int debug_level;
+
+#endif
