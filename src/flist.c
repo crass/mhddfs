5a4c2b1ae9e86e681947a0ab2144e42e04bc2d0f
dimka <dimka@d7d42532-9e51-4b1d-b199-9e4c94b77a30>
2009-07-01 05:38:08 +0000

    deadlock workaround
    git-svn-id: http://svn.uvw.ru/mhddfs/trunk@523 d7d42532-9e51-4b1d-b199-9e4c94b77a30

--- src/flist.c
+++ src/flist.c
@@ -18,19 +18,19 @@ static pthread_mutex_t files_lock;
 // init
 void flist_init(void)
 {
-    pthread_mutex_init(&files_lock, 0);
+	pthread_mutex_init(&files_lock, 0);
 }
 
 /* internal function */
 static void lock_files(void)
 {
-    pthread_mutex_lock(&files_lock);
+	pthread_mutex_lock(&files_lock);
 }
 
 /* internal function */
 static void unlock_files(void)
 {
-    pthread_mutex_unlock(&files_lock);
+	pthread_mutex_unlock(&files_lock);
 }
 
 
@@ -38,38 +38,38 @@ static void unlock_files(void)
 void flist_unlock_file(struct flist * item)
 {
 	mhdd_debug(MHDD_DEBUG, "flist_unlock_file: %s (%s)\n",
-		item->name, item->real_name);
+			item->name, item->real_name);
 	pthread_mutex_unlock(&item->lock);
 }
 
 static void flist_lock_file(struct flist * item)
 {
 	mhdd_debug(MHDD_DEBUG, "flist_lock_file: %s (%s)\n",
-	    item->name, item->real_name);
+			item->name, item->real_name);
 	pthread_mutex_lock(&item->lock);
 }
 
 // add file to list
-struct flist * flist_create(const char *name, 
-        const char *real_name, int flags, int fh)
+struct flist * flist_create(const char *name,
+		const char *real_name, int flags, int fh)
 {
-    struct flist * add=calloc(1, sizeof(struct flist));
+	struct flist * add=calloc(1, sizeof(struct flist));
 
-    add->flags=flags;
-    add->id=0; 
-    add->aid=add;
-    add->name=strdup(name);
-    add->real_name=strdup(real_name);
-    add->fh=fh;
-    pthread_mutex_init(&add->lock, 0);
+	add->flags=flags;
+	add->id=0;
+	add->aid=add;
+	add->name=strdup(name);
+	add->real_name=strdup(real_name);
+	add->fh=fh;
+	pthread_mutex_init(&add->lock, 0);
 	flist_lock_file(add);
-    lock_files();
+	lock_files();
 
-    add->next=files;
-    if (files) files->prev=add;
-    files=add;
-    unlock_files();
-    return add;
+	add->next=files;
+	if (files) files->prev=add;
+	files=add;
+	unlock_files();
+	return add;
 }
 
 /* return (malloced & locked) array for list files with name == name */
@@ -79,19 +79,19 @@ struct flist ** flist_items_by_eq_name(struct flist * info)
 	struct flist ** result;
 	int i, count;
 
-    mhdd_debug(MHDD_INFO, "flist_items_by_eq_name: %s\n", info->name);
+	mhdd_debug(MHDD_INFO, "flist_items_by_eq_name: %s\n", info->name);
 	lock_files();
 	for (next=files, count=0; next; next=next->next, count++);
-    if (!count) { unlock_files(); return 0; }
+	if (!count) { unlock_files(); return 0; }
 
-    result=calloc(count+1, sizeof(struct flist *));
+	result=calloc(count+1, sizeof(struct flist *));
 
-    for (next=files, i=0; next; next=next->next)
-    {
-        if (strcmp(info->name, next->name)==0) result[i++]=next;
-    	if (info==next) continue;
-	    flist_lock_file(next);
-    }
+	for (next=files, i=0; next; next=next->next)
+	{
+		if (strcmp(info->name, next->name)==0) result[i++]=next;
+		if (info==next) continue;
+		flist_lock_file(next);
+	}
 
 	unlock_files();
 
@@ -102,45 +102,45 @@ struct flist ** flist_items_by_eq_name(struct flist * info)
 /* return (locked) item from flist */
 struct flist * flist_item_by_id(uint64_t id)
 {
-    struct flist * next;
+	struct flist * next;
 	lock_files();
-    for(next=files; next; next=next->next)
-    {
-        if (next->id!=id) continue;
-	    flist_lock_file(next);
-        unlock_files();
-        return next;
-    }
-    unlock_files();
-    return 0;
+	for(next=files; next; next=next->next)
+	{
+		if (next->id!=id) continue;
+		flist_lock_file(next);
+		unlock_files();
+		return next;
+	}
+	unlock_files();
+	return 0;
 }
 
 
 /* internal function */
 static void delete_item(struct flist * item, int locked)
 {
-    struct flist *next;
-    lock_files();
+	struct flist *next;
+	lock_files();
 
-    for (next=files; next; next=next->next)
-    {
-        if (next==item) 
-        {
-        	if (!locked) flist_lock_file(item);
-            if (item->next) item->next->prev=item->prev;
-            if (item->prev) item->prev->next=item->next;
-            if (files==item) files=item->next;
+	for (next=files; next; next=next->next)
+	{
+		if (next==item)
+		{
+			if (!locked) flist_lock_file(item);
+			if (item->next) item->next->prev=item->prev;
+			if (item->prev) item->prev->next=item->next;
+			if (files==item) files=item->next;
 
-            mhdd_debug(MHDD_DEBUG, "delete_item: %s (%s)\n",
-                item->name, item->real_name);
-            free(item->name);
-            free(item->real_name);
-            free(item);
-            pthread_mutex_destroy(&item->lock);
-            break;
-        }
-    }
-    unlock_files();
+			mhdd_debug(MHDD_DEBUG, "delete_item: %s (%s)\n",
+					item->name, item->real_name);
+			free(item->name);
+			free(item->real_name);
+			free(item);
+			pthread_mutex_destroy(&item->lock);
+			break;
+		}
+	}
+	unlock_files();
 }
 
 
@@ -153,5 +153,5 @@ void flist_delete(struct flist * item)
 // delete locked file from list
 void flist_delete_locked(struct flist * item)
 {
-    delete_item(item, 1);
+	delete_item(item, 1);
 }
--- src/tools.c
+++ src/tools.c
@@ -1,21 +1,21 @@
 /*
-	mhddfs - Multi HDD [FUSE] File System
-	Copyright (C) 2008 Dmitry E. Oboukhov <dimka@avanto.org>
+   mhddfs - Multi HDD [FUSE] File System
+   Copyright (C) 2008 Dmitry E. Oboukhov <dimka@avanto.org>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-*/
+ */
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -35,356 +35,361 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // get diridx for maximum free space
 int get_free_dir(void)
 {
-    int i, max;
-    struct statvfs stf;
-    fsblkcnt_t max_space=0;
+	int i, max;
+	struct statvfs stf;
+	fsblkcnt_t max_space=0;
 
-    for (max=i=0; i<mhdd.cdirs; i++)
-    {
-        if (statvfs(mhdd.dirs[i], &stf)!=0) continue;
-        fsblkcnt_t space  = stf.f_bsize;
-        space *= stf.f_bavail;
+	for (max=i=0; i<mhdd.cdirs; i++)
+	{
+		if (statvfs(mhdd.dirs[i], &stf)!=0) continue;
+		fsblkcnt_t space  = stf.f_bsize;
+		space *= stf.f_bavail;
 
-        if (space>=mhdd.move_limit) return i;
+		if (space>=mhdd.move_limit) return i;
 
-        if(space>max_space)
-        {
-            max_space=space;
-            max=i;
-        }
-    }
+		if(space>max_space)
+		{
+			max_space=space;
+			max=i;
+		}
+	}
 
-    if (!max_space) 
-    {
-        mhdd_debug(MHDD_INFO,
-            "get_free_dir: Can not find freespace in all directories\n"
-        );
-        return -1;
-    }
-    return max;
+	if (!max_space)
+	{
+		mhdd_debug(MHDD_INFO,
+			"get_free_dir: Can't find freespace\n");
+		return -1;
+	}
+	return max;
 }
 
 // find mount point with free space > size
 // -1 if not found
 static int find_free_space(off_t size)
 {
-    int i, max;
-    struct statvfs stf;
-    fsblkcnt_t max_space=0;
+	int i, max;
+	struct statvfs stf;
+	fsblkcnt_t max_space=0;
 
-    for (max=-1,i=0; i<mhdd.cdirs; i++)
-    {
-        if (statvfs(mhdd.dirs[i], &stf)!=0) continue;
-        fsblkcnt_t space  = stf.f_bsize;
-        space *= stf.f_bavail;
+	for (max=-1,i=0; i<mhdd.cdirs; i++)
+	{
+		if (statvfs(mhdd.dirs[i], &stf)!=0) continue;
+		fsblkcnt_t space  = stf.f_bsize;
+		space *= stf.f_bavail;
 
-        if (space>size+mhdd.move_limit) return i;
+		if (space>size+mhdd.move_limit) return i;
 
-        if (space>size && (max<0 || max_space<space))
-        {
-            max_space=space;
-            max=i;
-        }
-    }
-    return max;
+		if (space>size && (max<0 || max_space<space))
+		{
+			max_space=space;
+			max=i;
+		}
+	}
+	return max;
 }
 
 static int reopen_files(struct flist * file, const char *new_name)
 {
 	int i;
-    struct flist ** rlist;
-    
-    mhdd_debug(MHDD_INFO, "reopen_files: %s -> %s\n",
-        file->real_name, new_name);
-    rlist = flist_items_by_eq_name(file);
-    if (!rlist) return 0;
+	struct flist ** rlist;
 
-    int error=0;
+	mhdd_debug(MHDD_INFO, "reopen_files: %s -> %s\n",
+			file->real_name, new_name);
+	rlist = flist_items_by_eq_name(file);
+	if (!rlist) return 0;
 
-    for (i=0; rlist[i]; i++)
-    {
-    	struct flist * next=rlist[i];
-        
-        off_t seek=lseek(next->fh, 0, SEEK_CUR);
-        int flags=next->flags;
-        int fh;
+	int error=0;
 
-        flags &= ~(O_EXCL|O_TRUNC);
+	for (i=0; rlist[i]; i++)
+	{
+		struct flist * next=rlist[i];
 
-        // open
-        if ((fh=open(new_name, flags))==-1)
-        {
-            mhdd_debug(MHDD_INFO, "reopen_files: error reopen: %s\n",
-                    strerror(errno));
-            if (!i) { error=errno; break; }
-            close(next->fh);
-        }
-        else
-        {
-            // seek
-            if (seek!=lseek(fh, seek, SEEK_SET))
-            {
-                mhdd_debug(MHDD_INFO, "reopen_files: error seek %s\n",
-                        strerror(errno));
-                close(fh);
-                if (!i) { error=errno; break; }
-            }
+		off_t seek=lseek(next->fh, 0, SEEK_CUR);
+		int flags=next->flags;
+		int fh;
 
-            // filehandle
-            if (dup2(fh, next->fh)!=next->fh)
-            {
-                mhdd_debug(MHDD_INFO, "reopen_files: error dup2 %s\n",
-                        strerror(errno));
-                close(fh);
-                if (!i) { error=errno; break; }
-            }
-            // close temporary filehandle
-            mhdd_debug(MHDD_MSG,
-                "reopen_files: reopened %s (to %s) old h=%x "
-                "new h=%x seek=%lld\n",
-                next->real_name, new_name, next->fh, fh, seek);
-            close(fh);
-        }
-    }
-    
-    /* unlock all files exclude current */
-    for (i=0; rlist[i]; i++)
-    {
-    	if (rlist[i]==file) continue;
-        flist_unlock_file(rlist[i]);
-    }
+		flags &= ~(O_EXCL|O_TRUNC);
 
-    free(rlist);
-    if (error) return -error;
-    return 0;
+		// open
+		if ((fh=open(new_name, flags))==-1)
+		{
+			mhdd_debug(MHDD_INFO,
+				"reopen_files: error reopen: %s\n",
+				strerror(errno));
+			if (!i) { error=errno; break; }
+			close(next->fh);
+		}
+		else
+		{
+			// seek
+			if (seek!=lseek(fh, seek, SEEK_SET))
+			{
+				mhdd_debug(MHDD_INFO,
+					"reopen_files: error seek %s\n",
+					strerror(errno));
+				close(fh);
+				if (!i) { error=errno; break; }
+			}
+
+			// filehandle
+			if (dup2(fh, next->fh)!=next->fh)
+			{
+				mhdd_debug(MHDD_INFO,
+					"reopen_files: error dup2 %s\n",
+					strerror(errno));
+				close(fh);
+				if (!i) { error=errno; break; }
+			}
+			// close temporary filehandle
+			mhdd_debug(MHDD_MSG,
+				"reopen_files: reopened %s (to %s) old h=%x "
+				"new h=%x seek=%lld\n",
+				next->real_name, new_name, next->fh, fh, seek);
+			close(fh);
+		}
+	}
+
+	/* unlock all files exclude current */
+	for (i=0; rlist[i]; i++)
+	{
+		if (rlist[i]==file) continue;
+		flist_unlock_file(rlist[i]);
+	}
+
+	free(rlist);
+	if (error) return -error;
+	return 0;
 }
 
 int move_file(struct flist * file, off_t wsize)
 {
-    char *from, *to, *buf;
-    off_t size;
-    FILE *input, *output;
-    int ret, dir_id;
+	char *from, *to, *buf;
+	off_t size;
+	FILE *input, *output;
+	int ret, dir_id;
 
-    mhdd_debug(MHDD_MSG, "move_file: %s\n", file->real_name);
+	mhdd_debug(MHDD_MSG, "move_file: %s\n", file->real_name);
 
-    from=file->real_name;
-    struct stat st;
-    if (stat(from, &st)!=0)
-    {
-        mhdd_debug(MHDD_MSG,
-            "move_file: error stat %s: %s\n", from, strerror(errno));
-        return -errno;
-    }
-    size=st.st_size;
-    if (size<wsize) size=wsize;
+	from=file->real_name;
+	struct stat st;
+	if (stat(from, &st)!=0)
+	{
+		mhdd_debug(MHDD_MSG, "move_file: error stat %s: %s\n",
+			from, strerror(errno));
+		return -errno;
+	}
+	size=st.st_size;
+	if (size<wsize) size=wsize;
 
-    if ((dir_id=find_free_space(size))==-1)
-    {
-    	mhdd_debug(MHDD_MSG, "move_file: can not find space\n");
-        return -1;
-    }
+	if ((dir_id=find_free_space(size))==-1)
+	{
+		mhdd_debug(MHDD_MSG, "move_file: can not find space\n");
+		return -1;
+	}
 
-    if (!(input=fopen(from, "r"))) return -errno;
-    
-    create_parent_dirs(dir_id, file->name);
+	if (!(input=fopen(from, "r"))) return -errno;
 
-    to=create_path(mhdd.dirs[dir_id], file->name);
-    if (!(output=fopen(to, "w+")))
-    {
-        ret=-errno;
-        mhdd_debug(MHDD_MSG, "move_file: error create %s: %s\n",
-                to, strerror(errno));
-        free(to);
-        fclose(input);
-        return(ret);
-    }
+	create_parent_dirs(dir_id, file->name);
 
-    mhdd_debug(MHDD_MSG, "move_file: move %s to %s\n", from, to);
+	to=create_path(mhdd.dirs[dir_id], file->name);
+	if (!(output=fopen(to, "w+")))
+	{
+		ret=-errno;
+		mhdd_debug(MHDD_MSG, "move_file: error create %s: %s\n",
+				to, strerror(errno));
+		free(to);
+		fclose(input);
+		return(ret);
+	}
 
-    // move data
-    buf=(char *)calloc(sizeof(char), MOVE_BLOCK_SIZE);
-    while((size=fread(buf, sizeof(char), MOVE_BLOCK_SIZE, input)))
-    {
-        if (size!=fwrite(buf, sizeof(char), size, output))
-        {
-            mhdd_debug(MHDD_MSG,
-                "move_file: error move data to %s: %s\n",
-                to, strerror(errno));
-            fclose(output);
-            fclose(input);
-            free(buf);
-            unlink(to);
-            free(to);
-            return -1;
-        }
-    }
-    free(buf);
+	mhdd_debug(MHDD_MSG, "move_file: move %s to %s\n", from, to);
 
-    mhdd_debug(MHDD_MSG, "move_file: done move data\n");
-    fclose(input);
+	// move data
+	buf=(char *)calloc(sizeof(char), MOVE_BLOCK_SIZE);
+	while((size=fread(buf, sizeof(char), MOVE_BLOCK_SIZE, input)))
+	{
+		if (size!=fwrite(buf, sizeof(char), size, output))
+		{
+			mhdd_debug(MHDD_MSG,
+				"move_file: error move data to %s: %s\n",
+				to, strerror(errno));
+			fclose(output);
+			fclose(input);
+			free(buf);
+			unlink(to);
+			free(to);
+			return -1;
+		}
+	}
+	free(buf);
 
-    // owner/group/permissions
-    fchmod(fileno(output), st.st_mode);
-    fchown(fileno(output), st.st_uid, st.st_gid);
-    fclose(output);
+	mhdd_debug(MHDD_MSG, "move_file: done move data\n");
+	fclose(input);
 
-    // time
-    struct utimbuf ftime={0};
-    ftime.actime=st.st_atime;
-    ftime.modtime=st.st_mtime;
-    utime(to, &ftime);
+	// owner/group/permissions
+	fchmod(fileno(output), st.st_mode);
+	fchown(fileno(output), st.st_uid, st.st_gid);
+	fclose(output);
 
-    if ((ret=reopen_files(file, to))==0) unlink(from);
-    else unlink(to);
+	// time
+	struct utimbuf ftime={0};
+	ftime.actime=st.st_atime;
+	ftime.modtime=st.st_mtime;
+	utime(to, &ftime);
 
-    mhdd_debug(MHDD_MSG,
-        "move_file: %s -> %s: done, code=%d\n", from, to, ret);
-    free(to);
-    return ret;
+	/* temporary workaround against deadlock */
+	/* TODO: fix it */
+/*         if ((ret=reopen_files(file, to))==0) unlink(from); */
+/*         else unlink(to); */
+	unlink(from);
+
+	mhdd_debug(MHDD_MSG, "move_file: %s -> %s: done, code=%d\n",
+		from, to, ret);
+	free(to);
+	return ret;
 }
 
 char * create_path(const char *dir, const char * file)
 {
-    if (file[0]=='/') file++;
-    int plen=strlen(dir);
-    int flen=strlen(file);
+	if (file[0]=='/') file++;
+	int plen=strlen(dir);
+	int flen=strlen(file);
 
-    char *path=(char *)calloc(plen+flen+2, sizeof(char));
+	char *path=(char *)calloc(plen+flen+2, sizeof(char));
 
-    if (dir[plen-1]=='/')
-    {
-        sprintf(path, "%s%s", dir, file);
-    }
-    else
-    {
-        sprintf(path, "%s/%s", dir, file);
-    }
+	if (dir[plen-1]=='/')
+	{
+		sprintf(path, "%s%s", dir, file);
+	}
+	else
+	{
+		sprintf(path, "%s/%s", dir, file);
+	}
 
-    plen=strlen(path);
-    if (plen>1 && path[plen-1]=='/') path[plen-1]=0;
+	plen=strlen(path);
+	if (plen>1 && path[plen-1]=='/') path[plen-1]=0;
 
-/*     mhdd_debug(MHDD_DEBUG, "create_path: %s\n", path); */
-    return(path);
+	/*     mhdd_debug(MHDD_DEBUG, "create_path: %s\n", path); */
+	return(path);
 }
 
 char * find_path(const char *file)
 {
-    int i;
-    struct stat st;
+	int i;
+	struct stat st;
 
-    for (i=0; i<mhdd.cdirs; i++)
-    {
-        char *path=create_path(mhdd.dirs[i], file);
-        if (lstat(path, &st)==0) return path;
-        free(path);
-    }
-    return 0;
+	for (i=0; i<mhdd.cdirs; i++)
+	{
+		char *path=create_path(mhdd.dirs[i], file);
+		if (lstat(path, &st)==0) return path;
+		free(path);
+	}
+	return 0;
 }
 
 int find_path_id(const char *file)
 {
-    int i;
-    struct stat st;
+	int i;
+	struct stat st;
 
-    for (i=0; i<mhdd.cdirs; i++)
-    {
-        char *path=create_path(mhdd.dirs[i], file);
-        if (lstat(path, &st)==0) 
-        {
-            free(path);
-            return i;
-        }
-        free(path);
-    }
-    return -1;
+	for (i=0; i<mhdd.cdirs; i++)
+	{
+		char *path=create_path(mhdd.dirs[i], file);
+		if (lstat(path, &st)==0)
+		{
+			free(path);
+			return i;
+		}
+		free(path);
+	}
+	return -1;
 }
 
 
 int create_parent_dirs(int dir_id, const char *path)
 {
-    mhdd_debug(MHDD_DEBUG,
-        "create_parent_dirs: dir_id=%d, path=%s\n", dir_id, path);
-    char *parent=get_parent_path(path);
-    if (!parent) return 0;
+	mhdd_debug(MHDD_DEBUG,
+		"create_parent_dirs: dir_id=%d, path=%s\n", dir_id, path);
+	char *parent=get_parent_path(path);
+	if (!parent) return 0;
 
-    char *exists=find_path(parent);
-    if (!exists) { free(parent); errno=EFAULT; return -errno; }
+	char *exists=find_path(parent);
+	if (!exists) { free(parent); errno=EFAULT; return -errno; }
 
 
-    char *path_parent=create_path(mhdd.dirs[dir_id], parent);
-    struct stat st;
+	char *path_parent=create_path(mhdd.dirs[dir_id], parent);
+	struct stat st;
 
-    // already exists
-    if (stat(path_parent, &st)==0)
-    {
-        free(exists);
-        free(path_parent);
-        free(parent);
-        return 0;
-    }
+	// already exists
+	if (stat(path_parent, &st)==0)
+	{
+		free(exists);
+		free(path_parent);
+		free(parent);
+		return 0;
+	}
 
-    // create parent dirs
-    int res=create_parent_dirs(dir_id, parent);
+	// create parent dirs
+	int res=create_parent_dirs(dir_id, parent);
 
-    if (res!=0)
-    {
-        free(path_parent);
-        free(parent); 
-        free(exists); 
-        return res; 
-    }
+	if (res!=0)
+	{
+		free(path_parent);
+		free(parent);
+		free(exists);
+		return res;
+	}
 
-    // get stat from exists dir
-    if (stat(exists, &st)!=0)
-    {
-        free(exists);
-        free(path_parent);
-        free(parent);
-        return -errno;
-    }
-    res=mkdir(path_parent, st.st_mode);
-    if (res==0)
-    {
-        chown(path_parent, st.st_uid, st.st_gid);
-        chmod(path_parent, st.st_mode);
-    }
-    else
-    {
-        res=-errno;
-        mhdd_debug(MHDD_DEBUG,
-            "create_parent_dirs: can not create dir %s: %s\n",
-                path_parent,
-                strerror(errno));
-    }
-    free(exists);
-    free(path_parent);
-    free(parent);
-    return res;
+	// get stat from exists dir
+	if (stat(exists, &st)!=0)
+	{
+		free(exists);
+		free(path_parent);
+		free(parent);
+		return -errno;
+	}
+	res=mkdir(path_parent, st.st_mode);
+	if (res==0)
+	{
+		chown(path_parent, st.st_uid, st.st_gid);
+		chmod(path_parent, st.st_mode);
+	}
+	else
+	{
+		res=-errno;
+		mhdd_debug(MHDD_DEBUG,
+			"create_parent_dirs: can not create dir %s: %s\n",
+			path_parent,
+			strerror(errno));
+	}
+	free(exists);
+	free(path_parent);
+	free(parent);
+	return res;
 }
 
 char * get_parent_path(const char * path)
 {
-    char *dir=strdup(path);
-    int len=strlen(dir);
-    if (len && dir[len-1]=='/') dir[--len]=0;
-    while(len && dir[len-1]!='/') dir[--len]=0;
-    if (len>1 && dir[len-1]=='/') dir[--len]=0;
-    if (len) return dir;
-    free(dir);
-    return 0;
+	char *dir=strdup(path);
+	int len=strlen(dir);
+	if (len && dir[len-1]=='/') dir[--len]=0;
+	while(len && dir[len-1]!='/') dir[--len]=0;
+	if (len>1 && dir[len-1]=='/') dir[--len]=0;
+	if (len) return dir;
+	free(dir);
+	return 0;
 }
 
 char * get_base_name(const char *path)
 {
-    char *dir=strdup(path);
-    int len=strlen(dir);
-    if (len && dir[len-1]=='/') dir[--len]=0;
-    char *file=strrchr(dir, '/');
-    if (file)
-    {
-        file++;
-        strcpy(dir, file);
-    }
-    return dir;
+	char *dir=strdup(path);
+	int len=strlen(dir);
+	if (len && dir[len-1]=='/') dir[--len]=0;
+	char *file=strrchr(dir, '/');
+	if (file)
+	{
+		file++;
+		strcpy(dir, file);
+	}
+	return dir;
 }
